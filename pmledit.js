298acd62520d356eba93af8d2e57a7265e77c71a
odonnec6 <odonnec6@tcd.ie>
2016-02-13 19:12:34 +0000

    file upload
    file upload didnt appear in last edit, I wasnt sure if this was a commit
    error on my part so I added it again this time commented out, needs
    Ubuntu 14.04 testing.

--- pmledit.js
+++ pmledit.js
@@ -112,3 +112,29 @@ function buttonPress(){
 	oPrntWin.document.write("<!doctype html><html><head><title>Print<\/title><\/head><body onload=\"print();\">" + oDoc.innerHTML + "<\/body><\/html>");
 	oPrntWin.document.close();
 }*/
+
+
+/*
+//code used for uploading a file
+  function readSingleFile(evt) {
+    //Retrieve the first (and only!) File from the FileList object
+    var f = evt.target.files[0]; 
+
+    if (f) {
+      var r = new FileReader();
+      r.onload = function(e) { 
+	      var contents = e.target.result;
+        alert( "Got the file.n" 
+              +"name: " + f.name + "n"
+              +"type: " + f.type + "n"
+              +"size: " + f.size + " bytesn"
+        );  
+      }
+      r.readAsText(f);
+    } else { 
+      alert("Failed to load file");
+    }
+  }
+
+  document.getElementById('fileinput').addEventListener('change', readSingleFile, false);
+  */
