d26230b20ba1b43c440880af8bc707721d1cdd81
odonnec6 <odonnec6@tcd.ie>
2016-02-20 03:17:14 +0000

    Removed old method
    I removed the old file uploading method

--- pmledit.js
+++ pmledit.js
@@ -173,27 +173,6 @@ function buttonPress(){
 }
 
 
-//code used for uploading a file
-function readSingleFile(evt) {
-	//Retrieve the first (and only!) File from the FileList object
-	var f = evt.target.files[0]; 
-
-	if (f) {
-	  var r = new FileReader();
-	  r.onload = function(e) { 
-	      var contents = e.target.result;
-	    alert( "Got the file.n" 
-	          +"name: " + f.name + "n"
-	          +"type: " + f.type + "n"
-	          +"size: " + f.size + " bytesn"
-	    );  
-	  }
-	  r.readAsText(f);
-	} else { 
-	  alert("Failed to load file");
-	}
-}
-
 function saveButton(){
 	//Grab the content of the editor
 	var text = editor.getValue();
