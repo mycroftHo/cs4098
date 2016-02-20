f32547015b0f867b0526366beedfc4cd33d0aea2
odonnec6 <odonnec6@tcd.ie>
2016-02-20 03:13:40 +0000

    Fixed File Upload
    fixed the file upload buttons so that it works with the codemirror
    editor

--- pmledit.js
+++ pmledit.js
@@ -226,3 +226,13 @@ function saveButton(){
 	}
 }
 
+function loadFileAsText(){
+	var fileToLoad = document.getElementById("fileToLoad").files[0];
+	var fileReader = new FileReader();
+	fileReader.onload = function(fileLoadedEvent){
+		var textFromFileLoaded = fileLoadedEvent.target.result;
+		document.getElementById("inputText").value = textFromFileLoaded;
+		editor.setValue(fileLoadedEvent.target.result);
+	};
+	fileReader.readAsText(fileToLoad,"UTF-8");
+}
