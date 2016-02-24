4753e859be864cb5256b028530108aa1457a2830
cartyse <carty_sean@hotmail.com>
2016-02-24 06:45:09 -0800

    oath.js

---
+++ passport-examples/oauth.js
@@ -0,0 +1,14 @@
+var ids = {
+  facebook: {
+    clientID: '1727336227501773',
+    clientSecret: '59525696561d7104ac9d8cbf244d3544',
+    callbackURL: 'http://localhost:1337/auth/facebook/callback'
+  },
+  google: {
+    clientID: '575965473375-aeajhf8lkvmg1hkkr5uqpp3akcf11et0.apps.googleusercontent.com',
+    clientSecret: 'zAefG1QGGc7Yj2X1NUIeTfob',
+    callbackURL: 'http://localhost:1337/auth/google/callback'
+  }
+ };
+
+module.exports = ids;
