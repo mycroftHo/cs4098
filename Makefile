2fc307bfed5c7f2ba455b16c1e9c105249167e05
ankerr <ankerr@tcd.ie>
2016-04-11 02:09:58 +0100

    Swimmy swim swim

--- Makefile
+++ Makefile
@@ -5,6 +5,7 @@ all:
 install: ; 
 	npm install express body-parser passport passport-facebook passport-google-oauth2 jade jsonfile vis; \
 	javac -cp .:swimlaneDrawer swimlaneDrawer/SocialDrawer.java; \
+	javac -cp .:swimlaneDrawer swimlaneDrawer/SwimlaneDrawer.java; \
 	mkdir ~/bin; \
 	mv swimlaneDrawer/*.class ~/bin; \
 	$(MAKE) -C pml;
--- swimlaneDrawer/SocialDrawer.java
+++ swimlaneDrawer/SocialDrawer.java
@@ -38,15 +38,14 @@ public class SocialDrawer{
     static int agentsSinceLastAction;
     static boolean firstAction;
     static Agent currentAgent;
-    static Canvas canvas;
     static SocialCanvas networkCanvas;
     static Action currentAction;
 
 
 
     private static void printAppropriateBoxes(List<String> csvLine, boolean lastLine){
-        if(csvLine.get(ENTRY_TYPE).equals("action") && 
-            firstAction && 
+        if(csvLine.get(ENTRY_TYPE).equals("action") &&
+            firstAction &&
             agentsSinceLastAction == 0){
             firstAction = false;
             actionName = csvLine.get(ENTRY_NAME);
@@ -55,19 +54,15 @@ public class SocialDrawer{
             actionList.add(currentAction);
             if(lastLine){
                 currentAgent = agentMap.get("NONE");
-                canvas.addAgent(currentAgent.getAgentNumber(), currentAgent.getName());
-                canvas.addAction(currentAgent.getAgentNumber(), actionNumber, actionName);
             }
         }
         //no agents in previous action
-        else if(csvLine.get(ENTRY_TYPE).equals("action") && 
+        else if(csvLine.get(ENTRY_TYPE).equals("action") &&
             !firstAction &&
             agentsSinceLastAction == 0){
             currentAgent = agentMap.get("NONE");
-            System.out.println("" + currentAgent.getName() + " number : "+  
+            System.out.println("" + currentAgent.getName() + " number : "+
                 currentAgent.getAgentNumber() + " carries out " + actionName);
-            canvas.addAgent(currentAgent.getAgentNumber(), currentAgent.getName());
-            canvas.addAction(currentAgent.getAgentNumber(), actionNumber, actionName);
             //Then update deets
             agentsSinceLastAction = 0;
             actionName = csvLine.get(ENTRY_NAME);
@@ -76,11 +71,9 @@ public class SocialDrawer{
             actionList.add(currentAction);
             //the reason it won't print this out is cos
             //this never gets called again once we hit the end of the file.
-            //if the last line is 
+            //if the last line is
             if(lastLine){
                 currentAgent = agentMap.get("NONE");
-                canvas.addAgent(currentAgent.getAgentNumber(), currentAgent.getName());
-                canvas.addAction(currentAgent.getAgentNumber(), actionNumber, actionName);
             }
         }
         else if(csvLine.get(ENTRY_TYPE).equals("action")){
@@ -91,8 +84,6 @@ public class SocialDrawer{
             actionList.add(currentAction);
             if(lastLine){
                 currentAgent = agentMap.get("NONE");
-                canvas.addAgent(currentAgent.getAgentNumber(), currentAgent.getName());
-                canvas.addAction(currentAgent.getAgentNumber(), actionNumber, actionName);
             }
         }
         //agent seen
@@ -101,13 +92,11 @@ public class SocialDrawer{
             if(!agentMap.containsKey(csvLine.get(ENTRY_NAME))){
                 currentAgent = new Agent(csvLine.get(ENTRY_NAME));
                 agentMap.put(currentAgent.getName(), currentAgent);
-                canvas.addAgent(currentAgent.getAgentNumber(), currentAgent.getName());
             }
             else{
                 currentAgent = agentMap.get(csvLine.get(ENTRY_NAME));
             }
             currentAction.addAgent(currentAgent);
-            canvas.addAction(currentAgent.getAgentNumber(), actionNumber, actionName);
             System.out.println("" + currentAgent.getName() + " number : "+
                 currentAgent.getAgentNumber() + " carries out " + actionName);
         }
@@ -122,7 +111,6 @@ public class SocialDrawer{
         }
         else{
             try{
-                canvas = new Canvas();
                 networkCanvas = new SocialCanvas();
                 actionsAndAgentsCSV = new CSVReader(args[0]);
                 actionName = "";
@@ -136,7 +124,6 @@ public class SocialDrawer{
                     List<String> csvLine = actionsAndAgentsCSV.getNextLine();
                     printAppropriateBoxes(csvLine, (i == actionsAndAgentsCSV.getSize() - 1));
                 }
-                canvas.FinishUp();
 
                 Agent[] agentArray = agentMap.values().toArray(new Agent[0]);
                 for(int i = 0; i < agentArray.length; i++){
@@ -162,6 +149,6 @@ public class SocialDrawer{
                 e.printStackTrace();
             }
         }
-        
+
     }
 }
